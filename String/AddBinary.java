163973992ccfa1bd7b59804db999019fb33bce94
ashishkumar47 <ashishkumar8163022@gmail.com>
2020-09-13 18:40:22 +0530

    September13:addBinaryOptimise added

--- String/AddBinary.java
+++ String/AddBinary.java
@@ -5,6 +5,32 @@ import java.io.*;
 
 
 public class AddBinary{
+
+	static String addBinaryOptimise(String str1,String str2){
+		String result="";
+		int carry=0;
+		int i=str1.length()-1,j=str2.length()-1;
+		int number=0;
+		while(i>=0||j>=0||number==1){
+			if(i>=0){
+			 number+=str1.charAt(i)-'0';
+			}
+			if(j>=0){
+			 number+=str2.charAt(j)-'0';
+			}
+			result=(number%2)+result;
+			number/=2;
+			i--;
+			j--;
+		}
+
+		return result;
+	}
+
+
+
+
+
 	static long convertToDecimal(String str){
 		long d=0,p=1;
 		for(int i=str.length()-1;i>=0;i--){
@@ -47,7 +73,7 @@ public class AddBinary{
 		for(int i=0;i<t;i++){
 			String str1=input.nextLine();
 			String str2=input.nextLine();
-			System.out.println(str1+"+"+str2+"->"+addBinary(str1,str2));
+			System.out.println(str1+"+"+str2+"->"+addBinaryOptimise(str1,str2));
 		}
 		
 	}
